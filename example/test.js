474f3bf7f89c71867d2b34058d08560a539d1d75
Saikat Das <saikatdas40g@gmail.com>
2025-06-23 17:09:06 +0530

    feat: update .gitignore to include src/extension.ts and modify test.js for consistency

--- example/test.js
+++ example/test.js
@@ -1,6 +1,10 @@
 
+//no track
+
 console.log('This is a test file in the example directory'); // no track
 
-console.log('This is a warning message in the test file. - test.js:4');
+console.log('This is a warning message in the test file. - test.js:6');
 
-console.log("");
+console.log("Hello"); //no track
+
+console.log("wd wdk wa wdd");
